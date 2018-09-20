37764ba553d932e279c623bf8599454a9076bf69
Amar Bhatt <bhatt.amar.a@gmail.com>
2018-09-19 21:38:51 -0400

    Fixed 120 degree bug

--- Robot_Simulator/ServoDriver.py
+++ Robot_Simulator/ServoDriver.py
@@ -9,5 +9,5 @@ class ServoDriver():
         ser = serial.Serial('COM' + str(com), 9600)
 
     def writeServo(joint, angle):
-        pwm = self.cal[port][0] + int((angle / (np.pi/3)) * (self.cal[port][1] - self.cal[port][0]))
+        pwm = self.cal[port][0] + int((angle / (2*np.pi/3)) * (self.cal[port][1] - self.cal[port][0]))
         self.ser.write((str(port).zfill(2) + str(pwm).zfill(4)).encode('utf-8'))
